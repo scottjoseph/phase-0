cd010f0ef9bde460fa3bc40ec1c82c37504faba0
scotjoseph <scottjoseph89@gmail.com>
2015-11-16 14:34:29 -0600

    add array_total_spec.rb

--- week-4/add-it-up/array_total_spec.rb
+++ week-4/add-it-up/array_total_spec.rb
@@ -1,25 +1,24 @@
 require_relative "my_solution"
 
-# describe 'total' do
-#   let(:array_1) { [1, 2, 3, 4, 5, 5, 7] }
-#   let(:array_2) { [4, 4, 5, 5, 6, 6, 6, 7] }
+ describe 'total' do
+   let(:array_1) { [1, 2, 3, 4, 5, 5, 7] }
+   let(:array_2) { [4, 4, 5, 5, 6, 6, 6, 7] }
 
-#   it "is defined as a method" do
-#     expect(defined? total).to eq 'method'
-#   end
+   it "is defined as a method" do
+     expect(defined? total).to eq 'method'
+   end
 
-#   it "requires a single argument" do
-#     expect(method(:total).arity).to eq 1
-#   end
+   it "requires a single argument" do
+     expect(method(:total).arity).to eq 1
+   end
+   it "returns the correct total of array 1" do
+     expect(total(array_1)).to eq 27
+   end
 
-#   it "returns the correct total of array 1" do
-#     expect(total(array_1)).to eq 27
-#   end
-
-#   it "returns the correct total of array 2" do
-#     expect(total(array_2)).to eq 43
-#   end
-# end
+   it "returns the correct total of array 2" do
+     expect(total(array_2)).to eq 43
+   end
+end
 
 describe 'sentence_maker' do
   let(:first_sentence) {["all", "my", "socks", "are", "dirty"]}
