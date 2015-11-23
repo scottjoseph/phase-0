adc21c1ef3952f35776643b4c3fed51d0f1ba4f7
scotjoseph <scottjoseph89@gmail.com>
2015-11-23 10:15:24 -0600

    modify my_solution with finished reflection

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -13,11 +13,11 @@
 
 # 0. Pseudocode
 
-# What is the input? 
+# What is the input?
 
 # - an array, a minimum size to pad the array with, and an option value that would fill the array
 
-# What is the output? 
+# What is the output?
 
 # - an array with more elements if the minimum size is greater than the size of the given array.
 
@@ -66,14 +66,20 @@ end
 
 # Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
 
+# - it was fairly easy to implement the destructive method. the non-destructive method was one of those things that I had trouble translating to code even though I knew was I needed the program to do.
+
 # Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+
+# - It was mainly figuring out how to do it non-destructively that gave me trouble but everything else passed. I knew I had to copy the array but I wasn't sure how.
 
 # When you refactored, did you find any existing methods in Ruby to clean up your code?
 
+# - the fill method was new to me.
+
 # How readable is your solution? Did you and your pair choose descriptive variable names?
+
+# - I think it's pretty readable. The names are descriptive albeit maybe generic.
 
 # What is the difference between destructive and non-destructive methods in your own words?
 
-
-
-
+# - a destructive method permanently modifies an element. a non-destructive method references the elements to create an output but doesn't change the data within the element themselves.
