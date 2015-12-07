bea3171a8ea49179a1334c852124c07944801e2b
scottjoseph <scottjosephj89@gmail.com>
2015-12-07 08:04:35 -0600

    Update group_project_solution.js

--- week-7/group_project_solution.js
+++ week-7/group_project_solution.js
@@ -1,5 +1,5 @@
-As a user, I want to find the sum of a bunch of numbers.
+// As a user, I want to find the sum of a bunch of numbers.
 
-As a user, I want to find the average or "mean" of a bunch of numbers.
+// As a user, I want to find the average or "mean" of a bunch of numbers.
 
-As a user, I want to find the the number in the middle of a bunch of numbers.
+// As a user, I want to find the the number in the middle of a bunch of numbers.
